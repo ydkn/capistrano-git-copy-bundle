5e8fd6adc11330d30e560e97dc8bad3c09f91fe7
Florian Schwab <me@ydkn.de>
2018-06-09 22:26:03 +0200

    move to gitlab

--- lib/capistrano/git_copy/bundle.rb
+++
@@ -1,3 +0,0 @@
-require 'capistrano/bundler'
-require 'capistrano/git_copy/bundle/utility'
-require 'capistrano/git_copy/bundle/task'
--- lib/capistrano/git_copy/bundle/version.rb
+++
@@ -1,7 +0,0 @@
-module Capistrano
-  module GitCopy
-    module Bundle
-      VERSION = '0.2.1'
-    end
-  end
-end
